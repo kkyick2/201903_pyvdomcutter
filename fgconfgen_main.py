a4cfde5bddbe2fa38f7332d39a9713aae6eb20eb
kkyick2 <kkyick2@gmail.com>
2019-04-11 02:00:18 +0800

    20190411_0200

--- fgconfgen_main.py
+++ fgconfgen_main.py
@@ -108,16 +108,15 @@ def gen_conf_policy(dictList, keys, vdom):
             write_row(outF, 'config vdom' + NEXTLINE)
             write_row(outF, 'edit ' + vdom + NEXTLINE)
             write_row(outF, 'config firewall policy' + NEXTLINE)
-
+            write_row(outF,  NEXTLINE)
             # loop for each policy in dictionary format, {'vdom:'root,'id':'999,'ippool':None,'uuid':'948a...' .}
             for policydict in dictList:
                 # loop for each key to follow the order
                 for key in keys:
                     if key in policydict.keys() and policydict[key]:
-                        # skip vdom column in xlsx
+                        # skip column in xlsx
                         if key == 'vdom':
                             continue
-                        # skip column in xlsx
                         elif key == 'uuid':
                             continue
                         elif key == 'Config Change Date':
@@ -125,8 +124,15 @@ def gen_conf_policy(dictList, keys, vdom):
                         # edit <id>
                         elif key == 'id':
                             write_row(outF, 'edit ' + str(policydict[key]) + NEXTLINE)
+                        # <name> and <comments> line add ""
                         elif key == 'name' or key == 'comments':
-                            write_row(outF, 'set ' + str(key) + ' "' + str(policydict[key]) + '"' + NEXTLINE)
+                            # remove object's next line and space from xls
+                            obj_list = str(policydict[key]).split()
+                            string = ""
+                            for obj in obj_list:
+                                string = string + obj
+                            write_row(outF, 'set ' + str(key) + ' "' + string + '"' + NEXTLINE)
+
                         elif key == 'srcaddr' or key == 'dstaddr' or key == 'service':
                             # remove object's next line and space from xls
                             obj_list = str(policydict[key]).split()
@@ -172,6 +178,7 @@ def gen_conf_service(list, vdom):
         write_row(outF, 'end' + NEXTLINE)
         write_row(outF, NEXTLINE)
         write_row(outF, 'config firewall service custom' + NEXTLINE)
+        write_row(outF, NEXTLINE)
         # loop for each item in list
         for item in list:
             # edit "<service name>"
@@ -204,10 +211,11 @@ def gen_conf_service(list, vdom):
     return
 
 
-def gen_conf_address(list, vdom):
+def gen_conf_address(list, vdom, addrDictList):
     """
         @param:	list: a list of address object in List format
                 vdom: vdom name for output "config vdom, edit <vdom>"
+                addrDictList: a List of Dict that contain associated-interface mapping
         @return: N/A
     """
     # output file name
@@ -228,8 +236,16 @@ def gen_conf_address(list, vdom):
         for item in list:
             # edit "<address name>"
             write_row(outF, 'edit "' + str(item) + '"' + NEXTLINE)
+
+            # set comment
             write_row(outF, 'set comment ' + '""' + NEXTLINE)
-            write_row(outF, 'set associated-interface ' + '"' + 'TBC' + '"' + NEXTLINE)
+
+            # set associated-interface
+            asso_int = 'TBC'
+            for i in addrDictList:
+                if item in i['addr']:
+                    asso_int = i['asso_int']
+            write_row(outF, 'set associated-interface ' + '"' + asso_int + '"' + NEXTLINE)
 
             # change to lower letter for checking, remarks: output is original letter
             item_lw = item.lower()
@@ -332,8 +348,8 @@ def xls2_obj_list(infile, sheet, vdom, key):
     returnList = []
     for dict in policy_list:
         returnList.append(dict[key])
-    logger1.info('***** Total: {} items in {} {}'.format(len(returnList),vdom,sheet))
-    print('***** Total: {} items in {} {}'.format(len(returnList),vdom,sheet))
+    logger1.info('***** {} {} -- Total: {} items'.format(vdom, sheet, len(returnList)))
+    print('***** {} {} -- Total: {} items'.format(vdom, sheet, len(returnList)))
     logger1.info('end')
     return returnList
 
@@ -365,161 +381,200 @@ def raw2_uniq_list(key, req_polList):
                     [ {'id' : '1', 'srcintf' : 'internal', ...}, {'id' : '2', 'srcintf' : 'external', ...}, ... ]
                 key: key of the target return list, eg, srcintf, dstintf
         @return
-                req_uniqList:  List of the target key
+                uniqList:       Type1: List of the target key
+                uniqDictList:  Type2: List of Dict of srcaddr/dstaddr
     """
     logger1 = logger.logger().get()
     print('start: raw2_uniq_list ' + key)
     logger1.info('start: raw2_uniq_list ' + key)
-    req_rawList = []
-    req_rawDict = {}
-    ykk_List = []
+
+    # Type1: for other key, return in List
+    rawList = [] # eg. ['Host_10.63.109.100', 'Host_10.63.109.101' ]
+
+    # Type2: for srcaddr/srcaddr key, return in List of Dict
+    rawDict = {} # eg. {'asso_int': u'E.TOF2', 'addr': 'Host_10.63.109.100'}
+    rawDictList = [] # eg. [{'asso_int': 'E.TOF2', 'addr': 'Host_10.63.109.100'}, {'asso_int': 'E.TOF2', 'addr': 'Host_10.63.109.101'}...]
+
+    # loop for each policy
     for dict in req_polList:
-        if 'srcaddr' in key:
-            req_rawList.append(str(dict[key]).split())
-            lll = []
-            for item in str(dict['srcaddr']).split():
-                lll.append(item.split('\n'))
-                print 'lll: ' + str(lll)
-                for i in lll:
-                    print i
-                    req_rawDict['addr'] = i
-                    req_rawDict['asso_int'] = dict['srcintf']
-                ykk_List.append(req_rawDict)
-            # req_rawDict['addr'] = dict['srcaddr']
-            # req_rawDict['asso_int'] = dict['srcintf']
-            print 'dictList: '+ str(ykk_List)
-        elif 'dstaddr' in key:
-            req_rawList.append(str(dict[key]).split())
-        else:
-            req_rawList.append(str(dict[key]).split())
+        # temp list to hold the key element, eg, addr objects
+        temp = str(dict[key]).split()
+        # print 'temp: ' + str(temp)
+        for i in temp:
+            # print 'i: ' + str(i)
+            # Type1: append each element to the raw List
+            rawList.append(i)
+            # Type2: if the key is srcaddr/dstaddr, we have to map it's associated interface
+            if 'srcaddr' in key:
+                rawDict['addr'] = i
+                rawDict['asso_int'] = dict['srcintf']
+                rawDictList.append(rawDict)
+                rawDict = {}
+                #print 'ykkList: ' + str(ykk_List)
+                #print 'rawList: ' + str(req_rawList)
+            elif 'dstaddr' in key:
+                rawDict['addr'] = i
+                rawDict['asso_int'] = dict['dstintf']
+                rawDictList.append(rawDict)
+                rawDict = {}
+                #print 'ykkList: ' + str(ykk_List)
+                #print 'rawList: ' + str(req_rawList)
 
-    req_flatList = flatten_list(req_rawList)
-    req_uniqList = remove_dup_list(req_flatList)
-    logger1.debug('before remove duplicate: {}'.format(req_flatList))
-    logger1.debug('after remove duplicate: {}'.format(req_uniqList))
-    logger1.info('*****{} obj, before remove duplicate: {} in xls, after remove duplicate: {}'.format(key,len(req_flatList),len(req_uniqList)))
+
+    uniqDictList = remove_dup_list(rawDictList)
+    uniqList = remove_dup_list(rawList)
+    logger1.debug('*****List: {}, b4 remove duplicate: {} in xls, after remove duplicate: {}'.format(key,len(rawList),len(uniqList)))
+    logger1.debug('*****Dict: {}, b4 remove duplicate: {} in xls, after remove duplicate: {}'.format(key,len(rawDictList),len(uniqDictList)))
+    # print('*****List: {}, b4 remove duplicate: {} in xls, after remove duplicate: {}'.format(key,len(rawList),len(uniqList)))
+    # print('*****Dict: {}, b4 remove duplicate: {} in xls, after remove duplicate: {}'.format(key,len(rawDictList),len(uniqDictList)))
     logger1.info('end')
-    return req_uniqList
+    return uniqList, uniqDictList
 
 
-def analyze_bas_mis(req_key, bas_List, req_uniqList):
+def analyze_bas_mis_obj(key, bas_l, req_l):
     """
         @param:
-                req_key: key of the operation string, eg,service
-                bas_List: baseline obj in List
-                req_uniqList: requirement obj in List
+                key: key of the operation string, eg,service
+                bas_l: baseline obj in List
+                req_l: requirement obj in List
         @return
                 missingObjList: find the requirement missing obj in baseline, return in list
     """
     logger1 = logger.logger().get()
-    print('--- [' + req_key + '] ----------------------------------------------------------')
-    print('start: analyze_bas_mis_obj: (key,bas_len,req_len) is ({},{},{})'.format(req_key,len(bas_List),len(req_uniqList)))
-    logger1.info('start: analyze_bas_mis_obj: (key,bas_len,req_len) is ({},{},{})'.format(req_key,len(bas_List),len(req_uniqList)))
+    print('--- [' + key + '] ----------------------------------------------------------')
+    print('start: analyze_bas_mis_obj: (key,bas_len,req_len) is ({},{},{})'.format(key,len(bas_l),len(req_l)))
+    logger1.info('start: analyze_bas_mis_obj: (key,bas_len,req_len) is ({},{},{})'.format(key,len(bas_l),len(req_l)))
 
     missingObjList = []
-    for req_item in req_uniqList:
-        if req_item in bas_List:
+    for i in req_l:
+        if i in bas_l:
             continue
         else:
-            missingObjList.append(req_item)
-            print 'Item not in baseline: {}'.format(req_item)
+            missingObjList.append(i)
+            print 'Item not in baseline: {}'.format(i)
 
     # analyze result
-    logger1.debug('baseline obj: {}'.format(bas_List))
-    logger1.debug('reqiremt obj: {}'.format(req_uniqList))
-    logger1.info('*** No of obj in (key,baseline,req,missing) is ({},{},{},{})'.format(req_key,len(bas_List),len(req_uniqList),len(missingObjList)))
+    # logger1.debug('baseline obj: {}'.format(bas_List))
+    # logger1.debug('reqiremt obj: {}'.format(req_uniqList))
+    logger1.info('*** No of obj in (key,baseline,req,missing) is ({},{},{},{})'.format(key,len(bas_l),len(req_l),len(missingObjList)))
     logger1.info('end')
     # print('*** baseline obj: {}'.format(bas_List))
     # print('*** reqiremt obj: {}'.format(req_uniqList))
-    print('*** No of obj in (key,baseline,req,missing) is ({},{},{},{})'.format(req_key,len(bas_List),len(req_uniqList),len(missingObjList)))
+    print('*** No of obj in (key,baseline,req,missing) is ({},{},{},{})'.format(key,len(bas_l),len(req_l),len(missingObjList)))
     print('-------------------------------------------------------------------------------')
     print('')
     return missingObjList
 
 
-def start():
+def analyze_bas_mis_route(bas_l, req_l):
+    return
+
+
+def start(base, req, vdom):
 
     # define logger
     logger1 = logger.logger().get()
     logger1.info('start script: '+__name__)
 
     # arg1/baseline, xls
-    bas_conf = 'CTFW03_20190324_0727.conf.xlsx'
+    bas_conf = base
     bas_conf_sheet = '07policy'
 
     # arg2-4/gen conf, xls requirement, vdom name
-    req_conf = 'CT03CASH2_20190328a.xlsx'
+    req_conf = req
     req_conf_sheet = 'Policy'
-    vdom = 'CT03CASH2'
+    vdom = vdom
 
     # define full path
     bas_conf_path = os.path.join(conf.FGCONFGEN_BAS_PATH, bas_conf)
     req_conf_path = os.path.join(conf.FGCONFGEN_REQ_PATH, req_conf)
-
-    print('======================= script arg input ==================================')
+    print('==================================================================================')
+    print('====================Begin: Script argument input =================================')
+    print('==================================================================================')
     print('=== baseline conf: {}'.format(bas_conf))
     print('=== requiremt xls: {}'.format(req_conf))
     print('=== requiremt tab: {}'.format(req_conf_sheet))
     print('=== doing vdom: {}'.format(vdom))
-    print('===========================================================================')
     print('')
 
-    # 1/baseline, xls to List for checking
-    print('====================== 1/Task: get baseline to list  ========================')
-    bas_intList = xls2_obj_list(bas_conf_path, '01int', vdom, 'name')
+    # A---prepare checking List
+    # baseline, xls to List elements
+    print('==================================================================================')
+    print('================== Task A1: Baseline to list =====================================')
     bas_zonList = xls2_obj_list(bas_conf_path, '02zone', vdom, 'name')
+    bas_rouList = xls2_obj_list(bas_conf_path, '03route', vdom, 'dst')
     bas_addList = xls2_obj_list(bas_conf_path, '04addr', vdom, 'name')
     bas_serList = xls2_obj_list(bas_conf_path, '06service', vdom, 'name')
     print('')
 
-    # 2/gen conf, xls requirement to txt
-    print('=================== 2/Task: gen policy conf ===============================')
+    # requirement, xls to List elements
+    print('=================== Task A2: Requirement to list =================================')
     req_polList, keys = xls2_policy_list(req_conf_path, req_conf_sheet, vdom)
+    print('')
+
+    # B---analyze baseline missing obj
+    print('==================================================================================')
+    print('=================== Task B: Find missing object in baseline ======================')
+    # ---------- task for service
+    key = 'service'
+    l, d = raw2_uniq_list(key, req_polList)
+    missing_service = analyze_bas_mis_obj(key, bas_serList, l)
+
+    # ---------- task for address
+    key = 'srcaddr'
+    l_srcaddr, d_srcaddr = raw2_uniq_list(key, req_polList)
+    key = 'dstaddr'
+    l_dstaddr, d_dstaddr = raw2_uniq_list(key, req_polList)
+
+    key = 'srcaddr_and_dstaddr'
+    addrList = l_srcaddr + l_dstaddr
+    addrDictList = d_srcaddr + d_dstaddr
+    # srcaddr_and_dstaddr may have duplicate items, remove them
+    addrList = remove_dup_list(addrList)
+    addrDictList = remove_dup_list(addrDictList)
+    missing_address = analyze_bas_mis_obj(key, bas_addList, addrList)
+
+    # ---------- task for zone
+    key = 'srcintf'
+    l_srcintf, d = raw2_uniq_list(key, req_polList)
+    key = 'dstintf'
+    l_dstintf, d = raw2_uniq_list(key, req_polList)
+    key = 'zone'
+    intfList = l_srcintf + l_dstintf
+    # srcintf and dstintf may have duplicate items
+    intfList = remove_dup_list(intfList)
+    missing_zone = analyze_bas_mis_obj(key, bas_zonList, intfList)
+    print('')
+
+    # analyze baseline missing route
+    print('=================== Task B: Find missing static route in baseline ================')
+    key = 'dst'
+    print missing_address
+
+    print('')
+    # C --- gen conf, policy
+    print('===================================================================================')
+    print('=================== Task C1: gen policy conf ======================================')
     gen_conf_policy(req_polList, keys, vdom)
     print('')
 
-    # 3/analyze baseline missing obj
-    print('=================== 3/Task: find missing object in baseline ===============')
-    # task for service
-    req_key = 'service'
-    req_uniqList = raw2_uniq_list(req_key, req_polList)
-    missing_service = analyze_bas_mis(req_key, bas_serList, req_uniqList)
-
-    # task for address
-    req_key = 'srcaddr'
-    req_uniqList_srcaddr = raw2_uniq_list(req_key, req_polList)
-    req_key = 'dstaddr'
-    req_uniqList_dstaddr = raw2_uniq_list(req_key, req_polList)
-    req_key = 'srcaddr_and_dstaddr'
-    addrList = req_uniqList_srcaddr + req_uniqList_dstaddr
-    # srcaddr_and_dstaddr may have duplicate items
-    addrList = remove_dup_list(addrList)
-    missing_address = analyze_bas_mis(req_key, bas_addList, addrList)
-
-    # task for zone
-    req_key = 'srcintf'
-    req_uniqList_srcintf = raw2_uniq_list(req_key, req_polList)
-    req_key = 'dstintf'
-    req_uniqList_dstintf = raw2_uniq_list(req_key, req_polList)
-    req_key = 'zone'
-    intfList = req_uniqList_srcintf + req_uniqList_dstintf
-    # srcintf and dstintf may have duplicate items
-    intfList = remove_dup_list(intfList)
-    missing_zone = analyze_bas_mis(req_key, bas_zonList, intfList)
-    print('')
-
     # 4/gen conf, missing service
-    print('=================== 4/Task: gen missing service conf ====================')
-    gen_conf_service(missing_service,vdom)
+    print('=================== Task C2: gen missing service conf ============================')
+    gen_conf_service(missing_service, vdom)
     print('')
 
     # 5/gen conf, missing address
-    print('=================== 5/Task: gen missing address conf ====================')
-    gen_conf_address(missing_address,vdom)
+    print('=================== Task C3: gen missing address conf ============================')
+    gen_conf_address(missing_address, vdom, addrDictList)
     print('')
 
     logger1.info('end: '+__name__)
 
 
 if __name__ == "__main__":
-    start()
+
+    bas_conf = 'CTFW03_20190324_0727.conf.xlsx'
+    req_conf = 'CT03CASH2_20190328a.xlsx'
+    vdom = 'CT03CASH2'
+
+    start(bas_conf, req_conf, vdom)
